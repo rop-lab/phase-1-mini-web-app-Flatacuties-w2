605a5f1838e6c2267485153b91a4d7af69b237b0
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-11 19:38:27 +0300

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-
-const url = "http://localhost:3000/characters"
-const animalDisplay = document.querySelector('#displayBar')
+let objGlobal 
+const renderCharacters = document.getElementById("animals")
+const renderImage = document.getElementById("animalImage")
+const rendertotalVotes =document.getElementById("Total Votes")
+const additionForm =document.getElementById("VotingBox")
+const voteInput = document.getElementById("VOTE")
+const url ="http://localhost:3000/characters"
+const animalDisplay = document.getElementById('displayBar')
 fetch (url)
 .then ((Response) =>Response.json())
 .then ((data)=>{
-console.log(data);
+    console.log(data);
+    renderAnimals(data[0])
+data.forEach((element) => {
+   animalList(element) 
+    
+});
+
 })
 function animalList(urlObj){
-let 
+let animalBar = document.createElement("li")
+animalBar.textContent = urlObj.name
+animalDisplay.append(animalBar)
+// add event listner to the node list
+//create a clickable event to the displayBar
+animalBar.addEventListener("click", ()=>{
+renderAnimals(urlObj)
+})
+}
+//function displaying the animals{
+function renderAnimals(urlObj){
+    renderCharacters.textContent=urlObj.name;
+    renderImage.src =urlObj.image;
+    rendertotalVotes.textContent=urlObj.votes;
+    objGlobal=urlObj
+    additionForm.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        objGlobal.votes +=parseInt(voteInput.value)
+        rendertotalVotes.textContent=objGlobal.votes
+        
+    })
+
+    
+
 }
