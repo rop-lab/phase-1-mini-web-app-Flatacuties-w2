07a111451c64381437c0ffb9b2bf8e5ce49de9da
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-17 16:08:26 +0300

    completed assignment

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 # As a user i can vote for the cutest animal from the list provide
 # When i click on a name of an animal displays the animal's name, image, and the number of votes
 # The application fetches a list of animals from a specified API(db.json), displays them on the webpage, and provides a voting mechanism.
+## Features
+
+# Fetch Data: 
+Utilizes the Fetch API to retrieve character data from `http://localhost:3000/characters`.
+# Dynamic Rendering:
+ Dynamically creates table rows to display character information.
+# Responsive Design: 
+Character images are displayed with a fixed width and height for a consistent and visually appealing presentation.
+
+## Overview
+The code fetches character data from `http://localhost:3000/characters` and dynamically creates table rows (<tr>) using the td_moringa function. The character details are then appended to the table body.
+
+The td_moringa function takes character details (name, id, image, votes) as parameters and creates a table row (<tr>) with corresponding table cells (<td>). This function encapsulates the HTML structure for displaying character information.
+
+
+
--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-let objGlobal 
-const renderCharacters = document.getElementById("animals")
-const renderImage = document.getElementById("animalImage")
-const rendertotalVotes =document.getElementById("Total Votes")
-const additionForm =document.getElementById("VotingBox")
-const voteInput = document.getElementById("VOTE")
-const url ="http://localhost:3000/characters"
-const animalDisplay = document.getElementById('displayBar')
-fetch (url)
-.then ((Response) =>Response.json())
-.then ((data)=>{
-    console.log(data);
-    renderAnimals(data[0])
-data.forEach((element) => {
-   animalList(element) 
-    
-});
+let tbody = document.getElementById('tbody')
 
+
+// create a fetch function
+fetch ("http://localhost:3000/characters")
+.then (res => res.json())
+
+.then(json=>{
+    json.map(data=>{
+    console.log(data)
+    tbody.append(td_moringa(data.name,data.id,data.image,data.votes));
 })
-function animalList(urlObj){
-let animalBar = document.createElement("li")
-animalBar.textContent = urlObj.name
-animalDisplay.append(animalBar)
-// add event listner to the node list
-//create a clickable event to the displayBar
-animalBar.addEventListener("click", ()=>{
-renderAnimals(urlObj)
 })
-}
-//function displaying the animals{
-function renderAnimals(urlObj){
-    renderCharacters.textContent=urlObj.name;
-    renderImage.src =urlObj.image;
-    rendertotalVotes.textContent=urlObj.votes;
-    objGlobal=urlObj
-    additionForm.addEventListener("submit", (e)=>{
-        e.preventDefault
-        objGlobal.votes +=parseInt(voteInput.value)
-        rendertotalVotes.textContent=objGlobal.votes
-        
-    })
 
-    
+// create td function 
+function td_moringa(name,id,img,vote){
+let td = document.createElement('tr');
 
+td.innerHTML = `
+    <td>${id}</td>
+    <td>${name}</td>
+    <td><img  src="${img}" style="width:90px; height: 100px;" ></td>
+    <td>${vote}</td>
+`;
+return td
 }
